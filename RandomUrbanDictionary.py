9d02e9dd598893aa6ce2e0e9e56049a5de075627
xhico <francgf@gmail.com>
2022-10-08 16:51:52 +0100

    Logging

--- RandomUrbanDictionary.py
+++ RandomUrbanDictionary.py
@@ -11,6 +11,7 @@ import yagmail
 import requests
 import tweepy
 import traceback
+import logging
 from bs4 import BeautifulSoup
 
 
@@ -18,19 +19,6 @@ def get911(key):
     with open('/home/pi/.911') as f:
         data = json.load(f)
     return data[key]
-
-
-CONSUMER_KEY = get911('TWITTER_URBAN_CONSUMER_KEY')
-CONSUMER_SECRET = get911('TWITTER_URBAN_CONSUMER_SECRET')
-ACCESS_TOKEN = get911('TWITTER_URBAN_ACCESS_TOKEN')
-ACCESS_TOKEN_SECRET = get911('TWITTER_URBAN_ACCESS_TOKEN_SECRET')
-EMAIL_USER = get911('EMAIL_USER')
-EMAIL_APPPW = get911('EMAIL_APPPW')
-EMAIL_RECEIVER = get911('EMAIL_RECEIVER')
-
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
 
 
 def getRandom():
@@ -49,18 +37,15 @@ def getRandom():
 def tweet(tweetStr):
     try:
         api.update_status(tweetStr)
-        print("Tweeted")
+        logger.info("Tweeted")
         return True
     except Exception as ex:
-        print(ex)
-        print("Failed")
+        logger.error(ex)
     return False
-
-    
 
 
 def favTweets(tags, numbTweets):
-    print("favTweets")
+    logger.info("favTweets")
     tags = tags.replace(" ", " OR ")
     tweets = tweepy.Cursor(api.search_tweets, q=tags).items(numbTweets)
     tweets = [tw for tw in tweets]
@@ -78,11 +63,11 @@ def main():
     # Get link, word, meaning, contributor and hashtags
     link, word, meaning, contributor = getRandom()
     hashtags = "#UrbanDictionary" + " " + "#" + word.replace(" ", "")
-    print(word)
-    # print(meaning)
-    # print(link)
-    # print(contributor)
-    # print(hashtags)
+    logger.info(word)
+    # logger.info(meaning)
+    # logger.info(link)
+    # logger.info(contributor)
+    # logger.info(hashtags)
 
     # Reduce meaning if necessary
     if len(word + "\n\n" + meaning + "\n\n" + "(" + contributor + ")" + "\n" + link + "\n" + hashtags) > 280:
@@ -94,20 +79,36 @@ def main():
     # Get tweets -> Like them
     favTweets(hashtags, 10)
 
+
 if __name__ == "__main__":
-    print("----------------------------------------------------")
-    print(str(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")))
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    
+    # Set Logging
+    LOG_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.path.abspath(__file__).replace(".py", ".log"))
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s", handlers=[logging.FileHandler(LOG_FILE), logging.StreamHandler()])
+    logger = logging.getLogger()
+
+    logger.info("----------------------------------------------------")
+
+    CONSUMER_KEY = get911('TWITTER_URBAN_CONSUMER_KEY')
+    CONSUMER_SECRET = get911('TWITTER_URBAN_CONSUMER_SECRET')
+    ACCESS_TOKEN = get911('TWITTER_URBAN_ACCESS_TOKEN')
+    ACCESS_TOKEN_SECRET = get911('TWITTER_URBAN_ACCESS_TOKEN_SECRET')
+    EMAIL_USER = get911('EMAIL_USER')
+    EMAIL_APPPW = get911('EMAIL_APPPW')
+    EMAIL_RECEIVER = get911('EMAIL_RECEIVER')
+
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth)
+
     # Check if script is already running
     procs = [proc for proc in psutil.process_iter(attrs=["cmdline"]) if os.path.basename(__file__) in '\t'.join(proc.info["cmdline"])]
     if len(procs) > 2:
-        print("isRunning")
+        logger.info("isRunning")
     else:
         try:
             main()
         except Exception as ex:
-            print(traceback.format_exc())
+            logger.error(traceback.format_exc())
             yagmail.SMTP(EMAIL_USER, EMAIL_APPPW).send(EMAIL_RECEIVER, "Error - " + os.path.basename(__file__), str(traceback.format_exc()))
         finally:
-            print("End")
+            logger.info("End")
